099ec219e34988f9ecbffa2d6d4040de3683ebcd
Waqar Ahmed <mailtowaqar1990@gmail.com>
2026-04-05 01:51:29 +0530

    added a tab switching code

---
+++ tests/tab-switching.spec.js
@@ -0,0 +1,37 @@
+import { test, expect, chromium } from "@playwright/test";
+
+test("Test case to demonstrate tab switching", async () => {
+  const browser = await chromium.launch();
+
+  // 2. Create the Context (The "Isolated Session")
+  const context = await browser.newContext();
+
+  // 3. Create a Page (The "Tab") within that context
+  const page = await context.newPage();
+
+  await page.goto("E:\\dummy_wesbites\\dummy.html");
+
+  // Code No. 1 - One way to handle the new tab 
+
+  //const newPagePromise= context.waitForEvent("page")
+
+  //await page.getByRole("button", { name: "Open Yahoo in New Tab" }).click();
+
+  // const newPage = await newPagePromise;
+  // await newPage.waitForLoadState();
+  // console.log("New page title  is: " + (await newPage.title()));
+  // await page.bringToFront();
+  // console.log("Current page title is: " + (await page.title()));
+
+  //Code No. 2 - will wait for the new tab to open and then switch to it
+  const [newPage] = await Promise.all([
+    context.waitForEvent("page"),
+    page.getByRole("button", { name: "Open Yahoo in New Tab" }).click(),
+  ]);
+  await newPage.waitForLoadState();
+  console.log("New page title  is: " + (await newPage.title()));
+  await page.bringToFront();
+  console.log("Current page title is: " + (await page.title()));
+
+  await page.pause();
+});
