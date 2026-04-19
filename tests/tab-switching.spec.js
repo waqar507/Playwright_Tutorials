485564328f2d503109a8831b34bf3e015faa8c82
Waqar Ahmed <mailtowaqar1990@gmail.com>
2026-04-20 00:55:45 +0530

    adding fixtures

--- tests/tab-switching.spec.js
+++ tests/tab-switching.spec.js
@@ -1,6 +1,6 @@
 import { test, expect, chromium } from "@playwright/test";
 
-test("Test case to demonstrate tab switching", async () => {
+test.skip("Test case to demonstrate tab switching", async () => {
   const browser = await chromium.launch();
 
   // 2. Create the Context (The "Isolated Session")
@@ -35,3 +35,17 @@ test("Test case to demonstrate tab switching", async () => {
 
   await page.pause();
 });
+
+
+test('Test case to demonstrate tab switching using fixtures', async ({page, context}) => {
+  await page.goto("E:\\dummy_wesbites\\dummy.html");
+
+  const [newPage] = await Promise.all([
+    context.waitForEvent("page"),
+    page.getByRole("button", { name: "Open Yahoo in New Tab" }).click(),
+  ]);
+  await newPage.waitForLoadState();
+  console.log("New page title  is: " + (await newPage.title()));
+  await page.bringToFront();
+  console.log("Current page title is: " + (await page.title()));
+})
