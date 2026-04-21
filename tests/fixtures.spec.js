baacf6dc289a3f78887c392f8ab123e2ff88e8ed
Waqar Ahmed <mailtowaqar1990@gmail.com>
2026-04-22 00:57:34 +0530

    latest fixture with beforeEach

--- tests/fixtures.spec.js
+++ tests/fixtures.spec.js
@@ -1,5 +1,29 @@
 import { test, expect } from '../fixtures/amazon-fixture.js';
 
-test('Example of opening Amazon Site and search using Custom Fixtures', async({loginAndSearch})=> {
+// Change this line
+test.beforeEach(async () => {
+    console.log("This will execute before each test case");
+     await page.goto("https://www.amazon.in/");
+     await page.getByRole("searchbox", { name: "Search Amazon.in" })
+                  .fill("Nike Shoes");
+     await page
+                .locator('[role="rowgroup"] div[role="button"]')
+                .filter({ hasText: "nike shoes for man running" })
+                .click();
+});
+
+test('Example of opening Amazon Site and search using Custom Fixtures 1', async({loginAndSearch})=> {
     await expect(loginAndSearch).toHaveTitle(/Nike/i);
+})
+
+test('Example of opening Amazon Site and search using Custom Fixtures 2', async({loginAndSearch})=> {
+    await expect(loginAndSearch).toHaveTitle(/Yahoo/i);
+})
+
+test('Example of opening Amazon Site and search using Custom Fixtures 3', async({loginAndSearch})=> {
+    await expect(loginAndSearch).toHaveTitle(/Jabong/i);
+})
+
+test('Example of opening Amazon Site and search using Custom Fixtures 4', async({loginAndSearch})=> {
+    
 })
